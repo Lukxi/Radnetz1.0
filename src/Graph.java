cb02d148160a754d6d24c94c530ce59e916d9cae
lukas.nowak <lukas.nowak03@outlook.de>
2021-04-20 09:15:17 +0200

    added kante and knoten methods

--- src/Graph.java
+++ src/Graph.java
@@ -3,43 +3,69 @@ public class Graph {
     public Knoten[] knotenfeld;
     private int anzahlKnoten;
     private int maxAnzahl;
-    int[][] adjazenmatrix;
+    private int[][] adjazenmatrix;
 
     public Graph(int maxAnzahlKnoten){
         adjazenmatrix = new int[maxAnzahlKnoten][maxAnzahlKnoten];
+        knotenfeld = new Knoten[maxAnzahlKnoten];
         anzahlKnoten=0;
         maxAnzahl = maxAnzahlKnoten;
         matrixVorbelegen();
     }
 
-    private void matrixVorbelegen(){
-        for (int i= 0; i < maxAnzahl; i++){
-            for (int a = 0; a < maxAnzahl; a++){
-                if (i == a){
-                    adjazenmatrix[i][a] = 0;
+
+    public void matrixVorbelegen() {
+        for (int i = 0; i < maxAnzahl; i = i + 1) {
+            for (int j = 0; j < maxAnzahl; j = j + 1) {
+                if (i == j) {
+                    adjazenmatrix[i][j] = 0;
+                } else {
+                    adjazenmatrix[i][j] = -1;
                 }
-                adjazenmatrix[i][a] = 0;
             }
         }
     }
 
-    private void adjazenmatrixAusgeben(){
-        System.out.println("");
+
+    public void adjazenmatrixAusgeben(){
+        System.out.println(" ");
         System.out.println("Die Adjazenzenmatrix lautet:");
 
-        for (int i = 0; i<maxAnzahl; i++) {
+        for (int i = 0; i<  maxAnzahl; i++) {
             for (int a = 0; i < maxAnzahl; i++){
                 System.out.print(adjazenmatrix[i][a] + ",");
         }
-        System.out.println("");
+        System.out.println(" ");
         }
     }
 
-    private void knotenAusgeben(){
+    public void knotenAusgeben(){
 
         for (int i = 0; i<anzahlKnoten; i++){
             knotenfeld[i].getDaten().ausgeben();
         }
+    }
+
+    public int knotenHiinzuefuzegen(Datenelement datenelement){
+
+        if (anzahlKnoten < knotenfeld.length) {
+            Knoten k = new Knoten(datenelement);
+            knotenfeld[anzahlKnoten] = k;
+            anzahlKnoten++;
+            return anzahlKnoten;
+        }else {
+            System.out.println("Es wurde kein Knoten erzeugt, da der Graph schon die max. Anzahl an Knoten enthält!");
+            return -1;
+        }
+    }
+
+    public void kanteHinzufuegen(int start, int Ziel, int bewertung){
+
+        if (start > maxAnzahl || Ziel > maxAnzahl){
+            System.out.println("Start oder Zielwert nicht gefunden!");
+        }else
+            adjazenmatrix[start][Ziel] = bewertung;
+
     }
 
 }
